f2a05aaa3deea70f546469f180c14ff907abacb0
Camilla Devasini <camilladevasini@gmail.com>
2024-06-22 15:41:30 +0200

    fix slaves call

--- Server.js
+++ Server.js
@@ -22,7 +22,7 @@ app.get(`/user/:userId/settings`, (req, res) => {
   const userId = req.params.userId;
   res.status(200).send({
     userId: userId,
-    designId: 1,
+    designId: 2,
     role: "admin",
   });
 });
@@ -46,21 +46,144 @@ app.get(`/user/:userId`, (req, res) => {
 
 app.get("/faq", (req, res) => {
   res.status(200).send({
-    faq: [
+    title: "Benvenuto nella sezione FAQ",
+    description: "Qui potresti trovare quello che stai cercando.",
+    faqList: [
       {
-        question: "How do I reset my password?",
-        answer: "Click on the 'Forgot password?' link on the login page.",
-      },
-      {
-        question: "Can I change my email address?",
+        id: 0,
+        question: "Come posso fare acquisti",
         answer:
-          "Yes, you can change your email address in your account settings.",
+          "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
       },
       {
-        question: "How do I delete my account?",
-        answer: "You can delete your account in your account settings.",
+        id: 1,
+        question: "Dove compro i crediti",
+        answer:
+          "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+      },
+      {
+        id: 2,
+        question: "Diritto al rimborso",
+        answer:
+          "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+      },
+      {
+        id: 3,
+        question: "domanda 4",
+        answer:
+          "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+      },
+      {
+        id: 4,
+        question: "domanda 5",
+        answer:
+          "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+      },
+      {
+        id: 5,
+        question: "domanda 6",
+        answer:
+          "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+      },
+      {
+        id: 6,
+        question: "domanda 7",
+        answer:
+          "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+      },
+      {
+        id: 7,
+        question: "domanda 8",
+        answer:
+          "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
       },
     ],
+  });
+});
+
+// Admin's slaves for the dashboard
+const allSlavesData = [
+  {
+    id: 1,
+    firstName: "Mario",
+    lastName: "Rossi",
+    email: "prova@Rossi.prova",
+    phone: "3348558857",
+    status: "pending",
+  },
+  {
+    id: 2,
+    firstName: "Giovanni",
+    lastName: "Verdi",
+    email: "prova@Verdi.prova",
+    phone: "3234558857",
+    status: "banned",
+  },
+  {
+    id: 3,
+    firstName: "Luca",
+    lastName: "Gialli",
+    email: "prova@Gialli.prova",
+    phone: "3348552342",
+    status: "active",
+  },
+  {
+    id: 4,
+    firstName: "Mario",
+    lastName: "Neri",
+    email: "prova@Neri.prova",
+    phone: "3348558857",
+    status: "active",
+  },
+  {
+    id: 5,
+    firstName: "Luca",
+    lastName: "Viola",
+    email: "prova@Neri.prova",
+    phone: "3348558857",
+    status: "active",
+  },
+];
+
+app.get("/slaves", (req, res) => {
+  const { page, search, statusFilter } = req.query;
+  const limit = 3; // number of items per page
+
+  let filteredData = allSlavesData;
+
+  if (search) {
+    filteredData = allSlavesData.filter((item) => {
+      const lowerCaseSearch = search.toLowerCase();
+      return (
+        item.firstName.toLowerCase().includes(lowerCaseSearch) ||
+        item.lastName.toLowerCase().includes(lowerCaseSearch) ||
+        item.email.toLowerCase().includes(lowerCaseSearch) ||
+        item.phone.toLowerCase().includes(lowerCaseSearch)
+      );
+    });
+  }
+  if (statusFilter) {
+    statusFilter === "all"
+      ? filteredData
+      : (filteredData = filteredData.filter(
+          (item) => item.status === statusFilter
+        ));
+  }
+
+  const resultsCount = filteredData.length;
+  const totalPages = Math.ceil(resultsCount / limit);
+  const offset = (Number(page) - 1) * limit;
+  const paginatedResults = filteredData.slice(offset, offset + limit);
+  console.log("pageNumber", page);
+
+  res.status(200).send({
+    data: paginatedResults,
+    pagination: {
+      activePage: Number(page),
+      itemsPerPage: limit,
+      totalPages: totalPages,
+      totalItems: resultsCount,
+    },
   });
 });
 
@@ -70,7 +193,7 @@ app.post("/login", (req, res) => {
   console.log(req.body);
   // se email e password sono uguali a test@test e pass123 ovvero quelli che ho impostato io come corretti
   // allora mi aspetto che il backend esegua una chiamata che restituisca i dati dell'utente e il suo jwt token
-  if (email === "test@test.com" && password === "pass123") {
+  if (email === "qq@qq.it" && password === "1111") {
     // creo il token jwt contenente userId e role
     const token = jwt.sign(userData, "my_secret_key", {
       expiresIn: "1h",
@@ -82,7 +205,7 @@ app.post("/login", (req, res) => {
       sameSite: "Strict",
       maxAge: 3600000, // 1h
     });
-    console.log("Set-Cookie header:", res.get("Set-Cookie"));
+    // console.log("Set-Cookie header:", res.get("Set-Cookie"));
     res.status(200).json({
       userData,
       token,
@@ -94,11 +217,15 @@ app.post("/login", (req, res) => {
 
 app.post(`/logout/${userData.userId}`, (req, res) => {
   res.clearCookie("token");
-  res.status(200).send("User logged out");
+  res.status(200).send({
+    isLoggedOut: true,
+  });
 });
 
 app.post("/register", (req, res) => {
-  res.status(200).send("New user request registered");
+  res.status(200).send({
+    isRegistered: true,
+  });
 });
 
 // PUT
