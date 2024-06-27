36498c434e4ce15618f3e57f8d7b5d27bf0f28a6
Camilla Devasini <camilladevasini@gmail.com>
2024-06-27 21:25:20 +0200

    add api

--- Server.js
+++ Server.js
@@ -6,8 +6,436 @@ const cookieParser = require("cookie-parser");
 const app = express();
 app.use(cookieParser());
 let cors = require("cors");
-const port = 3001;
 
+const port = 3001;
+// Admin's slaves for the dashboard
+const allSlavesData = [
+  {
+    id: 1,
+    firstName: "Mario",
+    lastName: "Rossi",
+    email: "mario@rossi.com",
+    phone: "3348558857",
+    status: "pending",
+    projects: [
+      {
+        id: 1,
+        date: "2021-10-10",
+        status: "completed",
+        brand: "Fiat",
+        model: "500",
+        generation: "2010",
+        engine: "2.0",
+        engineHp: "200",
+        year: "2010",
+        gearbox: "automatic",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+      {
+        id: 2,
+        date: "2021-10-09",
+        status: "pending",
+        brand: "Toyota",
+        model: "Aygo",
+        generation: "2019",
+        engine: "2.0",
+        engineHp: "200",
+        year: "2010",
+        gearbox: "manual",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+      {
+        id: 3,
+        date: "2021-10-08",
+        status: "pending",
+        brand: "Toyota",
+        model: "Z4X",
+        generation: "2018",
+        engine: "2.0",
+        engineHp: "100",
+        year: "2010",
+        gearbox: "automatic",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+      {
+        id: 4,
+        date: "2021-10-07",
+        status: "refunded",
+        brand: "Fiat",
+        model: "600",
+        generation: "2010",
+        engine: "2.0",
+        engineHp: "200",
+        year: "2010",
+        gearbox: "automatic",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+      {
+        id: 5,
+        date: "2021-10-10",
+        status: "failed",
+        brand: "BMW",
+        model: "Serie 1",
+        generation: "2010",
+        engine: "2.0",
+        engineHp: "200",
+        year: "2010",
+        gearbox: "manual",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+    ],
+  },
+  {
+    id: 2,
+    firstName: "Giovanni",
+    lastName: "Verdi",
+    email: "giovanni@verdi.it",
+    phone: "3234558857",
+    status: "banned",
+    projects: [
+      {
+        id: 1,
+        date: "2021-10-10",
+        status: "completed",
+        brand: "brand1",
+        model: "Stelvio",
+        generation: "2010",
+        engine: "2.0",
+        engineHp: "127",
+        year: "2010",
+        gearbox: "automatic",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+      {
+        id: 2,
+        date: "2021-10-10",
+        status: "new",
+        brand: "brand1",
+        model: "Stelvio",
+        generation: "2010",
+        engine: "2.0",
+        engineHp: "200",
+        year: "2010",
+        gearbox: "automatic",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+      {
+        id: 3,
+        date: "2021-10-10",
+        status: "pending",
+        brand: "brand1",
+        model: "Stelvio",
+        generation: "2010",
+        engine: "2.0",
+        engineHp: "200",
+        year: "2010",
+        gearbox: "automatic",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+      {
+        id: 4,
+        date: "2021-10-10",
+        status: "refunded",
+        brand: "brand1",
+        model: "Stelvio",
+        generation: "2010",
+        engine: "2.0",
+        engineHp: "200",
+        year: "2010",
+        gearbox: "automatic",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+      {
+        id: 5,
+        date: "2021-10-10",
+        status: "failed",
+        brand: "brand1",
+        model: "Stelvio",
+        generation: "2010",
+        engine: "2.0",
+        engineHp: "200",
+        year: "2010",
+        gearbox: "automatic",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+    ],
+  },
+  {
+    id: 3,
+    firstName: "Luca",
+    lastName: "Gialli",
+    email: "luca@gialli.com",
+    phone: "3348552342",
+    status: "active",
+    projects: [
+      {
+        id: 1,
+        date: "2021-10-10",
+        status: "completed",
+        brand: "brand1",
+        model: "Stelvio",
+        generation: "2010",
+        engine: "2.0",
+        engineHp: "200",
+        year: "2010",
+        gearbox: "automatic",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+      {
+        id: 2,
+        date: "2021-10-10",
+        status: "new",
+        brand: "brand1",
+        model: "Stelvio",
+        generation: "2010",
+        engine: "2.0",
+        engineHp: "200",
+        year: "2010",
+        gearbox: "automatic",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+      {
+        id: 3,
+        date: "2021-10-10",
+        status: "pending",
+        brand: "brand1",
+        model: "Stelvio",
+        generation: "2010",
+        engine: "2.0",
+        engineHp: "200",
+        year: "2010",
+        gearbox: "automatic",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+      {
+        id: 4,
+        date: "2021-10-10",
+        status: "refunded",
+        brand: "brand1",
+        model: "Stelvio",
+        generation: "2010",
+        engine: "2.0",
+        engineHp: "200",
+        year: "2010",
+        gearbox: "automatic",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+      {
+        id: 5,
+        date: "2021-10-10",
+        status: "failed",
+        brand: "brand1",
+        model: "Stelvio",
+        generation: "2010",
+        engine: "2.0",
+        engineHp: "200",
+        year: "2010",
+        gearbox: "automatic",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+    ],
+  },
+  {
+    id: 4,
+    firstName: "Mario",
+    lastName: "Neri",
+    email: "mario@neri.it",
+    phone: "3348558857",
+    status: "active",
+    projects: [
+      {
+        id: 1,
+        date: "2021-10-10",
+        status: "completed",
+        brand: "brand1",
+        model: "Stelvio",
+        generation: "2010",
+        engine: "2.0",
+        engineHp: "200",
+        year: "2010",
+        gearbox: "automatic",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+      {
+        id: 2,
+        date: "2021-10-10",
+        status: "new",
+        brand: "brand1",
+        model: "Stelvio",
+        generation: "2010",
+        engine: "2.0",
+        engineHp: "200",
+        year: "2010",
+        gearbox: "automatic",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+      {
+        id: 3,
+        date: "2021-10-10",
+        status: "pending",
+        brand: "brand1",
+        model: "Stelvio",
+        generation: "2010",
+        engine: "2.0",
+        engineHp: "200",
+        year: "2010",
+        gearbox: "automatic",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+      {
+        id: 4,
+        date: "2021-10-10",
+        status: "refunded",
+        brand: "brand1",
+        model: "Stelvio",
+        generation: "2010",
+        engine: "2.0",
+        engineHp: "200",
+        year: "2010",
+        gearbox: "automatic",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+      {
+        id: 5,
+        date: "2021-10-10",
+        status: "failed",
+        brand: "brand1",
+        model: "Stelvio",
+        generation: "2010",
+        engine: "2.0",
+        engineHp: "200",
+        year: "2010",
+        gearbox: "automatic",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+    ],
+  },
+  {
+    id: 5,
+    firstName: "Luca",
+    lastName: "Viola",
+    email: "luca@viola.it",
+    phone: "3348558857",
+    status: "active",
+    projects: [
+      {
+        id: 1,
+        date: "2021-10-10",
+        status: "completed",
+        brand: "brand1",
+        model: "Stelvio",
+        generation: "2010",
+        engine: "2.0",
+        engineHp: "200",
+        year: "2010",
+        gearbox: "automatic",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+      {
+        id: 2,
+        date: "2021-10-10",
+        status: "new",
+        brand: "brand1",
+        model: "Stelvio",
+        generation: "2010",
+        engine: "2.0",
+        engineHp: "200",
+        year: "2010",
+        gearbox: "automatic",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+      {
+        id: 3,
+        date: "2021-10-10",
+        status: "pending",
+        brand: "brand1",
+        model: "Stelvio",
+        generation: "2010",
+        engine: "2.0",
+        engineHp: "200",
+        year: "2010",
+        gearbox: "automatic",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+      {
+        id: 4,
+        date: "2021-10-10",
+        status: "refunded",
+        brand: "brand1",
+        model: "Stelvio",
+        generation: "2010",
+        engine: "2.0",
+        engineHp: "200",
+        year: "2010",
+        gearbox: "automatic",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+      {
+        id: 5,
+        date: "2021-10-10",
+        status: "failed",
+        brand: "brand1",
+        model: "Stelvio",
+        generation: "2010",
+        engine: "2.0",
+        engineHp: "200",
+        year: "2010",
+        gearbox: "automatic",
+        readingMethod: "Magicmotorsport",
+        extraOptions: "Only options --adblue",
+        price: 100,
+      },
+    ],
+  },
+];
 app.use(cors());
 app.use(bodyParser.json());
 app.get("/", (req, res) => {
@@ -101,52 +529,8 @@ app.get("/faq", (req, res) => {
   });
 });
 
-// Admin's slaves for the dashboard
-const allSlavesData = [
-  {
-    id: 1,
-    firstName: "Mario",
-    lastName: "Rossi",
-    email: "prova@Rossi.prova",
-    phone: "3348558857",
-    status: "pending",
-  },
-  {
-    id: 2,
-    firstName: "Giovanni",
-    lastName: "Verdi",
-    email: "prova@Verdi.prova",
-    phone: "3234558857",
-    status: "banned",
-  },
-  {
-    id: 3,
-    firstName: "Luca",
-    lastName: "Gialli",
-    email: "prova@Gialli.prova",
-    phone: "3348552342",
-    status: "active",
-  },
-  {
-    id: 4,
-    firstName: "Mario",
-    lastName: "Neri",
-    email: "prova@Neri.prova",
-    phone: "3348558857",
-    status: "active",
-  },
-  {
-    id: 5,
-    firstName: "Luca",
-    lastName: "Viola",
-    email: "prova@Neri.prova",
-    phone: "3348558857",
-    status: "active",
-  },
-];
-
 app.get("/slaves", (req, res) => {
-  const { page, search, statusFilter } = req.query;
+  const { page = 1, search, statusFilter } = req.query;
   const limit = 3; // number of items per page
 
   let filteredData = allSlavesData;
@@ -174,7 +558,6 @@ app.get("/slaves", (req, res) => {
   const totalPages = Math.ceil(resultsCount / limit);
   const offset = (Number(page) - 1) * limit;
   const paginatedResults = filteredData.slice(offset, offset + limit);
-  console.log("pageNumber", page);
 
   res.status(200).send({
     data: paginatedResults,
@@ -187,13 +570,86 @@ app.get("/slaves", (req, res) => {
   });
 });
 
+app.get("/slaves/:slaveId", (req, res) => {
+  const slaveId = req.params.slaveId;
+  const slave = allSlavesData.find((item) => item.id === Number(slaveId));
+  res.status(200).send(slave);
+});
+
+app.get(`/slaves/:slaveId/projects`, (req, res) => {
+  const slaveId = req.params.slaveId;
+  const { page = 1, status, brand, model, date, extraOptions } = req.query;
+  const limit = 3;
+
+  // Find the slave by id
+  const slave = allSlavesData.find((slave) => slave.id === Number(slaveId));
+
+  if (!slave) {
+    return res.status(404).send({
+      message: `Slave with id ${slaveId} not found`,
+    });
+  }
+
+  let filteredProjects = slave.projects;
+
+  filteredProjects = filteredProjects.filter((project) => {
+    return (
+      (status !== "all" ? project.status === status : true) &&
+      (brand !== "all" ? project.brand === brand : true) &&
+      (model !== "all" ? project.model === model : true) &&
+      (date !== "all" ? project.date === date : true) &&
+      (extraOptions !== "all" ? project.extraOptions === extraOptions : true)
+    );
+  });
+
+  const resultsCount = filteredProjects.length;
+  const totalPages = Math.ceil(resultsCount / limit);
+  const validPage = Math.max(1, Math.min(Number(page), totalPages));
+
+  const offset = (validPage - 1) * limit;
+  const paginatedResults = filteredProjects.slice(offset, offset + limit);
+
+  res.status(200).send({
+    notFilteredData: slave.projects,
+    data: paginatedResults,
+    pagination: {
+      activePage: Number(page),
+      itemsPerPage: limit,
+      totalPages: totalPages,
+      totalItems: resultsCount,
+    },
+  });
+});
+app.get("/slaves/:slaveId/projects/:projectId", (req, res) => {
+  const { slaveId, projectId } = req.params;
+  console.log("slaveId", slaveId);
+  console.log("projectId", projectId);
+  const slave = allSlavesData.find((slave) => slave.id === Number(slaveId));
+  if (slave) {
+    console.log("slave", slave);
+    const project = slave.projects.find(
+      (project) => project.id === Number(projectId)
+    );
+    if (project) {
+      return res.status(200).send(project);
+    } else {
+      return res.status(404).send({
+        message: `Project with id ${projectId} not found`,
+      });
+    }
+  } else {
+    return res.status(404).send({
+      message: `Slave with id ${slaveId} not found`,
+    });
+  }
+});
 // POST
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
   console.log(req.body);
   // se email e password sono uguali a test@test e pass123 ovvero quelli che ho impostato io come corretti
   // allora mi aspetto che il backend esegua una chiamata che restituisca i dati dell'utente e il suo jwt token
-  if (email === "test@test.com" && password === "pass123") {
+  if (email === "test@test.com" && password === "qqqqq") {
     // creo il token jwt contenente userId e role
     const token = jwt.sign(userData, "my_secret_key", {
       expiresIn: "1h",
@@ -244,6 +700,33 @@ app.put(`/user/:userId`, (req, res) => {
     vatNumber: "12345678901",
     phone: "3334445555",
   });
+});
+
+app.put(`/slaves/:slaveId/projects/:projectId`, (req, res) => {
+  const { slaveId, projectId } = req.params;
+  const { status } = req.body;
+  console.log("request of body", req.body);
+
+  const slave = allSlavesData.find((item) => item.id == slaveId);
+
+  if (slave) {
+    const project = slave.projects.find((project) => project.id == projectId);
+    if (project) {
+      project.status = status;
+
+      res.status(200).send({
+        updatedProject: project,
+      });
+    } else {
+      res.status(404).send({
+        message: `Project with id ${projectId} not found`,
+      });
+    }
+  } else {
+    res.status(404).send({
+      message: `Slave with id ${slaveId} not found`,
+    });
+  }
 });
 
 app.listen(port, () => {
