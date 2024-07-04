9cc51c42c3579e4443f62a16e73b2337b69c0143
Camilla Devasini <camilladevasini@gmail.com>
2024-07-04 09:47:15 +0200

    update backend

--- Server.js
+++ Server.js
@@ -12,11 +12,20 @@ const port = 3001;
 const allSlavesData = [
   {
     id: 1,
+    storeId: 1, // lo store Id indica l'admin a cui è associato lo slave
     firstName: "Mario",
     lastName: "Rossi",
     email: "mario@rossi.com",
     phone: "3348558857",
+    companyName: "Auto23",
+    address: "35 via bernardino",
+    city: "bergamo",
+    zipCode: "24100",
+    vatNumber: "IT849948474",
     status: "pending",
+    password: null,
+    passwordTemporary: "Ahfj!2-E3rh4du?!Qlb2",
+    isActive: false,
     projects: [
       {
         id: 1,
@@ -97,11 +106,19 @@ const allSlavesData = [
   },
   {
     id: 2,
+    storeId: 1, // lo store Id indica l'admin a cui è associato lo slave
     firstName: "Giovanni",
     lastName: "Verdi",
     email: "giovanni@verdi.it",
+    companyName: "Autooos",
+    address: "via Roma 2",
+    city: "Milano",
+    zipCode: "34483",
+    vatNumber: "IT849948474",
     phone: "3234558857",
     status: "banned",
+    password: null,
+    passwordTemporary: "Ahfj!23445.Erhdu",
     projects: [
       {
         id: 1,
@@ -182,11 +199,19 @@ const allSlavesData = [
   },
   {
     id: 3,
+    storeId: 1, // lo store Id indica l'admin a cui è associato lo slave
     firstName: "Luca",
     lastName: "Gialli",
     email: "luca@gialli.com",
+    companyName: "Ciaociao",
+    address: "via Lodi 1",
+    city: "Roma",
+    zipCode: "24100",
+    vatNumber: "IT849948474",
     phone: "3348552342",
     status: "active",
+    password: null,
+    passwordTemporary: "Ahfj!23445.Erhdu",
     projects: [
       {
         id: 1,
@@ -267,11 +292,19 @@ const allSlavesData = [
   },
   {
     id: 4,
+    storeId: 1, // lo store Id indica l'admin a cui è associato lo slave
     firstName: "Mario",
     lastName: "Neri",
     email: "mario@neri.it",
+    companyName: "Autoyay",
+    address: "via Garibaldi 11",
+    city: "Padova",
+    zipCode: "24103",
+    vatNumber: "IT849948474",
     phone: "3348558857",
     status: "active",
+    password: null,
+    passwordTemporary: "Ahfj!23445.Erhdu",
     projects: [
       {
         id: 1,
@@ -352,11 +385,19 @@ const allSlavesData = [
   },
   {
     id: 5,
+    storeId: 1, // lo store Id indica l'admin a cui è associato lo slave
     firstName: "Luca",
     lastName: "Viola",
     email: "luca@viola.it",
     phone: "3348558857",
-    status: "active",
+    companyName: "V-auto",
+    address: "via Manzoni 45",
+    city: "Siracusa",
+    zipCode: "24130",
+    vatNumber: "IT849948474",
+    status: "pending",
+    password: null,
+    passwordTemporary: "Ahfj!23445.Erhdu",
     projects: [
       {
         id: 1,
@@ -531,7 +572,7 @@ app.get("/faq", (req, res) => {
 
 app.get("/slaves", (req, res) => {
   const { page = 1, search, statusFilter } = req.query;
-  const limit = 3; // number of items per page
+  const limit = 4; // number of items per page
 
   let filteredData = allSlavesData;
 
@@ -574,12 +615,15 @@ app.get("/slaves/:slaveId", (req, res) => {
   const slaveId = req.params.slaveId;
   const slave = allSlavesData.find((item) => item.id === Number(slaveId));
   res.status(200).send(slave);
+  // selezione dei dati: escludo: id, projects, password,
+  // se lo status è pending, devo fare anche una chiamata alla tabella slaves_temporary con l'id dello slave per ottenere la password temporanea
+  // per il primo accesso
 });
 
 app.get(`/slaves/:slaveId/projects`, (req, res) => {
   const slaveId = req.params.slaveId;
   const { page = 1, status, brand, model, date, extraOptions } = req.query;
-  const limit = 3;
+  const limit = 4;
 
   // Find the slave by id
   const slave = allSlavesData.find((slave) => slave.id === Number(slaveId));
@@ -682,6 +726,45 @@ app.post("/register", (req, res) => {
   res.status(200).send({
     isRegistered: true,
   });
+  // questa chiamata avviene quando uno slave fa la richiesta di accesso.
+  // lo slave deve essere inserito:
+  // - nella tabella slaves con lo status pending
+  // - nella tabella slaves_temporary con la password temporanea
+  // la funzione per generare tale password è già implementata:
+
+  // const generateRandomPassword = (length = 12) => {
+  //   const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  //   const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  //   const numberChars = "0123456789";
+  //   const specialChars = "!@#$%^&*()_+[]{}|;:,.<>?";
+
+  //   const allChars =
+  //     uppercaseChars + lowercaseChars + numberChars + specialChars;
+
+  //   let password = "";
+
+  //   // Ensure at least one character from each category
+  //   password +=
+  //     uppercaseChars[Math.floor(Math.random() * uppercaseChars.length)];
+  //   password +=
+  //     lowercaseChars[Math.floor(Math.random() * lowercaseChars.length)];
+  //   password += numberChars[Math.floor(Math.random() * numberChars.length)];
+  //   password += specialChars[Math.floor(Math.random() * specialChars.length)];
+
+  //   // Fill the rest of the password
+  //   for (let i = password.length; i < length; i++) {
+  //     const randomIndex = Math.floor(Math.random() * allChars.length);
+  //     password += allChars[randomIndex];
+  //   }
+
+  //   // Shuffle the password
+  //   password = password
+  //     .split("")
+  //     .sort(() => 0.5 - Math.random())
+  //     .join("");
+
+  //   return password;
+  // };
 });
 
 // PUT
@@ -722,6 +805,24 @@ app.put(`/slaves/:slaveId/projects/:projectId`, (req, res) => {
         message: `Project with id ${projectId} not found`,
       });
     }
+  } else {
+    res.status(404).send({
+      message: `Slave with id ${slaveId} not found`,
+    });
+  }
+});
+app.put(`/slaves/:slaveId`, (req, res) => {
+  const { slaveId } = req.params;
+
+  const slave = allSlavesData.find((item) => item.id == slaveId);
+
+  if (slave) {
+    Object.entries(req.body).forEach(([key, value]) => {
+      slave[key] = value;
+    });
+    return res.status(200).send({
+      updatedSlave: slave,
+    });
   } else {
     res.status(404).send({
       message: `Slave with id ${slaveId} not found`,
